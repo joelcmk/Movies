d0e5970345e666b8ada32b9d953f1192b10e8977
joelcmk <57118300+joelcmk@users.noreply.github.com>
2025-04-18 19:02:20 -0500

    Added PostHog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { PostHogProvider } from 'src/components/PostHogProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,8 +32,10 @@ export default function RootLayout({
  bg-white dark:bg-black // Example: white in light, black in dark
 `}
       >
-        {children}
-        <Analytics />
+        <PostHogProvider>
+          {children}
+          <Analytics />
+        </PostHogProvider>
       </body>
     </html>
   );
